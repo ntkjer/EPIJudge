e98bcd18422d5194a5e1b3b5ccf8ba9a0e7407bb
ntkjer <ntkjer@gmail.com>
2020-08-12 14:36:59 -0400

    revised solution

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,10 +2,22 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
+    """
+    Tn = O(k)
+    
+    Explanation:
+    x &= x - 1 is equal to x with the lowest set bit erased.
+
+    If  1.  x       = 00101100
+        2.  x - 1   = 00101011
+        3. [1 & 2]  = 00101000
+    
+    This means that we only go through the loop for the amount of 1 bits,k, in n-length bit sequence.
+    """
     result = 0
     while x:
-        result ^= x & 1
-        x >>= 1
+        result ^= 1
+        x &= x - 1
     return result
 
 
