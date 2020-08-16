5d7250d6fbec1aaf3e2217704c69d8ef9e8a2073
ntkjer <ntkjer@gmail.com>
2020-08-16 11:15:50 -0400

    optimal sol

--- problem_mapping.js
+++ problem_mapping.js
@@ -24,7 +24,7 @@ problem_mapping = {
                 "total": 10000
             },
             "Python: parity.py": {
-                "passed": 0,
+                "passed": 10000,
                 "total": 10000
             }
         },
@@ -38,7 +38,7 @@ problem_mapping = {
                 "total": 10001
             },
             "Python: swap_bits.py": {
-                "passed": 0,
+                "passed": 10001,
                 "total": 10001
             }
         },
@@ -52,7 +52,7 @@ problem_mapping = {
                 "total": 10000
             },
             "Python: reverse_bits.py": {
-                "passed": 0,
+                "passed": 10000,
                 "total": 10000
             }
         },
@@ -65,8 +65,8 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
-                "passed": 0,
+            "Python: closest_int_same_weight.py": {
+                "passed": 10000,
                 "total": 10000
             }
         },
